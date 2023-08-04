3887c5d2881144b6150510c741cc6d3d8296a1af
Tim Wahrburg <t.wahrburg@gmail.com>
2023-08-04 11:55:26 +0200

    fix: Removed the top-level-adding out of closeAnimation

--- cuddly-cards/Assets/Main/Script/Card/Animation/Main/CloseAnimation.cs
+++ cuddly-cards/Assets/Main/Script/Card/Animation/Main/CloseAnimation.cs
@@ -12,25 +12,11 @@ public class CloseAnimation : CardAnimation
     public override Sequence GetAnimationSequence(CardNode activeNode, CardNode baseNode)
     {
         Sequence entireSequence = DOTween.Sequence();
-        CardNode rootNode = _cardManager.RootNode;
-        _cardManager.AddToTopLevelMainPile(baseNode);
 
         entireSequence.Join(_subAnimations.MoveNodeYLiftPile(baseNode, baseNode));
 
-        if (baseNode != rootNode)
-        {
-            _cardManager.AddToTopLevelMainPile(baseNode.Parent);
-
-            if (baseNode.Parent != rootNode)
-            {
-                _cardManager.AddToTopLevelMainPile(rootNode);
-            }
-        }
-
         foreach (CardNode childNode in baseNode.Children)
         {
-            _cardManager.AddToTopLevelMainPile(childNode);
-
             entireSequence.Join(DOTween.Sequence()
                 .Append(_subAnimations.MoveNodeYLiftPile(childNode, baseNode))
                 .Append(_subAnimations.MoveNodeXToLeft(childNode)));
