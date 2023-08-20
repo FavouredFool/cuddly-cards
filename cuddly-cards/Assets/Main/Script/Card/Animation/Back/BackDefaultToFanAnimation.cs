bc13503fd1a0ca291a0ff55a246f1933b21fb8da
Tim Wahrburg <t.wahrburg@gmail.com>
2023-08-20 20:05:01 +0200

    feat: It works!

--- cuddly-cards/Assets/Main/Script/Card/Animation/Back/BackDefaultToFanAnimation.cs
+++ cuddly-cards/Assets/Main/Script/Card/Animation/Back/BackDefaultToFanAnimation.cs
@@ -113,12 +113,17 @@ public class BackDefaultToFanAnimation : BackParentAnimation
 
             foreach (CardNode childChild in child.Children)
             {
+                _cardManager.AddToTopLevel(childChild);
+
                 // The height of all the children below them, but only the children. So you take cards below and subtract the amount of other children to the left of basenode
-                int height = childChild.GetNodeCountUpToNodeInPile(activeNode, CardTraversal.CONTEXT) - child.Parent.Children.IndexOf(baseNode);
+                int countUp = childChild.GetNodeCountUpToNodeInPile(activeNode, CardTraversal.CONTEXT);
+                int subtract = child.Parent.Children.Count - (child.Parent.Children.IndexOf(child) + 1);
+
+                int height = countUp - subtract;
 
                 sequence.Join(DOTween.Sequence()
                     .Append(_subAnimations.MoveNodeYLiftPile(childChild, activeNode))
-                    .Append(_subAnimations.MoveNodeZNearer(activeNode))
+                    .Append(_subAnimations.MoveNodeZNearer(childChild))
                     .AppendInterval(_waitTime + _horizontalTime)
                     .Append(_subAnimations.MoveNodeY(childChild, height))
                     );
@@ -127,165 +132,4 @@ public class BackDefaultToFanAnimation : BackParentAnimation
 
         return sequence;
     }
-
-
-
-    //------
-    /*
-
-    public override Tween AnimateChildrenAndBase(CardNode activeNode, CardNode baseNode)
-    {
-        Sequence sequence = DOTween.Sequence();
-
-        //sequence.Join(SetOtherChildren(activeNode, baseNode));
-        //sequence.Join(SetBaseChild(activeNode, baseNode));
-
-        return sequence;
-    }
-
-    public Tween SetBaseChild(CardNode activeNode, CardNode baseNode)
-    {
-        Sequence sequence = DOTween.Sequence();
-
-        sequence.Append(FromDone(activeNode, baseNode));
-
-        _cardManager.AddToTopLevel(baseNode);
-        int index = activeNode.Children.IndexOf(baseNode);
-        int count = activeNode.Children.Count;
-
-        sequence.Join(DOTween.Sequence()
-            .Append(BaseChildTweenIn(baseNode, activeNode))
-            .AppendInterval(_horizontalTime + _waitTime)
-            .Append(BaseChildTweenOut(baseNode, index, count)));
-
-        
-        foreach (CardNode childchild in baseNode.Children)
-        {
-            _cardManager.AddToTopLevel(childchild);
-
-            sequence.Join(DOTween.Sequence()
-                .Append(ChildTweenIn(childchild, activeNode))
-                .AppendInterval(_waitTime)
-                .Append(ChildChildTweenOut()));
-        }
-        
-
-        return sequence;
-    }
-
-    public Tween FromDone(CardNode activeNode, CardNode baseNode)
-    {
-        Sequence sequence = DOTween.Sequence();
-
-        
-
-        if (_fromFan)
-        {
-            sequence.Append(_subAnimations.FanIn(baseNode, activeNode));
-        }
-        else
-        {
-            Sequence fromSubSequence = DOTween.Sequence();
-
-            fromSubSequence.Join(_subAnimations.MoveNodeYLiftPile(baseNode, activeNode));
-
-            foreach (CardNode child in baseNode.Children)
-            {
-                _cardManager.AddToTopLevel(child);
-
-                fromSubSequence.Join(ChildTweenIn(child, activeNode));
-            }
-
-            sequence.Append(fromSubSequence);
-        }
-        
-
-        return sequence;
-    }
-
-
-    public Tween BaseChildTweenIn(CardNode baseNode, CardNode activeNode)
-    {
-        return _subAnimations.MoveNodeYLiftPile(baseNode, activeNode);
-    }
-
-    public Tween BaseChildTweenOut(CardNode baseNode, int index, int count)
-    {
-        if (_toFan)
-        {
-            return _subAnimations.FanOutCard(baseNode, index, count, false);
-        }
-        else
-        {
-            return DOTween.Sequence()
-                .Append(_subAnimations.MoveNodeXToChild(baseNode, baseNode))
-                .Append(_subAnimations.MoveNodeYLowerPile(baseNode));
-        }
-    }
-
-    public Tween ChildTweenIn(CardNode childchild, CardNode activeNode)
-    {
-        return DOTween.Sequence()
-            .Append(_subAnimations.MoveNodeYLiftPile(childchild, activeNode))
-            .Append(_subAnimations.MoveNodeXToLeft(childchild));
-    }
-
-    public Tween ChildChildTweenOut()
-    {
-        if (_toFan)
-        {
-            return DOTween.Sequence();
-        }
-        else
-        {
-            return DOTween.Sequence();
-        }
-    }
-
-
-    // Other children
-
-    public Tween SetOtherChildren(CardNode activeNode, CardNode baseNode)
-    {
-        Sequence sequence = DOTween.Sequence();
-
-        Tween tweenForEnd;
-
-        int count = activeNode.Children.Count;
-        for (int i = 0; i < count; i++)
-        {
-            CardNode child = activeNode.Children[i];
-
-            if (child == baseNode)
-            {
-                break;
-            }
-
-            _cardManager.AddToTopLevel(child);
-
-            sequence.Join(DOTween.Sequence()
-            .Append(_subAnimations.MoveNodeYLiftPile(child, activeNode))
-            .Append(_subAnimations.MoveNodeZNearer(child))
-            .AppendInterval(_waitTime)
-            .Append(OtherChildrenTweenOut(child, i, count)));
-        }
-
-        return sequence;
-    }
-
-    public Tween OtherChildrenTweenOut(CardNode node, int index, int count)
-    {
-        if (_toFan)
-        {
-            return _subAnimations.FanOutCard(node, index, count, false);
-        }
-        else
-        {
-            return DOTween.Sequence()
-            .Append(_subAnimations.MoveNodeXToChild(node, node))
-            .Append(_subAnimations.MoveNodeYLiftPile(node, node));
-        }
-    }
-
-    */
 }
