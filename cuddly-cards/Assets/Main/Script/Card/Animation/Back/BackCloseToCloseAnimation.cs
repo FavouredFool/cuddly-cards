4056126ab3bfad1350a44d5ba8890fe8bd69198c
Tim Wahrburg <t.wahrburg@gmail.com>
2023-08-21 14:05:11 +0200

    feat: Created BackCloseToCloseAnimation

---
+++ cuddly-cards/Assets/Main/Script/Card/Animation/Back/BackCloseToCloseAnimation.cs
@@ -0,0 +1,70 @@
+
+using DG.Tweening;
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.Collections.Generic;
+using static CardInfo;
+
+public class BackCloseToCloseAnimation : BackParentAnimation
+{
+
+    public BackCloseToCloseAnimation(CardManager cardManager) : base(cardManager)
+    {
+    }
+
+    public override Tween SetActiveNode(CardNode activeNode, CardNode baseNode)
+    {
+        return DOTween.Sequence()
+            .Append(_subAnimations.MoveNodeYLiftPile(activeNode, activeNode))
+            .Append(_subAnimations.MoveNodeZNearer(activeNode))
+            .AppendInterval(_waitTime + _horizontalTime)
+            .Append(_subAnimations.MoveNodeY(activeNode, activeNode.GetNodeCount(CardTraversal.CONTEXT) - baseNode.GetNodeCount(CardTraversal.CONTEXT)));
+    }
+
+    public override Tween AnimateChildrenAndBase(CardNode activeNode, CardNode baseNode)
+    {
+        Sequence sequence = DOTween.Sequence();
+
+        sequence.Join(SetBaseNode(activeNode, baseNode));
+        sequence.Join(SetOtherChildren(activeNode, baseNode));
+
+        return sequence;
+    }
+
+    public Tween SetBaseNode(CardNode activeNode, CardNode baseNode)
+    {
+        // BaseNode is being turned into a child.
+        _cardManager.AddToTopLevel(baseNode);
+
+        return DOTween.Sequence()
+            .Append(_subAnimations.MoveNodeYLiftPile(baseNode, activeNode))
+            .AppendInterval(_horizontalTime + _waitTime)
+            .Append(_subAnimations.MoveNodeXToChild(baseNode, baseNode))
+            .Append(_subAnimations.MoveNodeY(baseNode, baseNode.GetNodeCount(CardTraversal.CONTEXT)));
+    }
+
+    public Tween SetOtherChildren(CardNode activeNode, CardNode baseNode)
+    {
+        Sequence sequence = DOTween.Sequence();
+
+        foreach (CardNode child in activeNode.Children)
+        {
+            if (child == baseNode)
+            {
+                continue;
+            }
+
+            // Other children
+            _cardManager.AddToTopLevel(child);
+
+            sequence.Join(DOTween.Sequence()
+            .Append(_subAnimations.MoveNodeYLiftPile(child, activeNode))
+            .Append(_subAnimations.MoveNodeZNearer(child))
+            .AppendInterval(_waitTime + _horizontalTime + _verticalTime)
+            );
+        }
+
+        return sequence;
+    }
+}
