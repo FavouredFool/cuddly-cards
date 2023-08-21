2295b7104f2f656d1c82552a24a738576b610786
Tim Wahrburg <t.wahrburg@gmail.com>
2023-08-21 11:03:58 +0200

    feat: Probably fixed back-ANimation problem? Not quite sure tbh

--- cuddly-cards/Assets/Main/Script/Card/Animation/Back/BackParentAnimation.cs
+++ cuddly-cards/Assets/Main/Script/Card/Animation/Back/BackParentAnimation.cs
@@ -39,7 +39,7 @@ public abstract class BackParentAnimation : MainAnimation
 
         if (backToBe != rootNode)
         {
-            int rootHeight = rootNode.GetNodeCount(CardTraversal.BODY);
+            int rootHeight = rootNode.GetNodeCount(CardTraversal.BODY) - backToBe.GetNodeCount(CardTraversal.BODY);
 
             List<CardNode> lowerTopMostCardsRoot = backToBe.GetTopNodesBelowNodeInPile(rootNode, CardTraversal.BODY);
 
