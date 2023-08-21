7831df3eed0c240a3bed8236e3c6dbdf7aa04d9f
Tim Wahrburg <t.wahrburg@gmail.com>
2023-08-21 10:49:09 +0200

    feat: Fixed FanToNoChildren!

--- cuddly-cards/Assets/Main/Script/Card/Animation/Child/FanToNoChildrenAnimation.cs
+++ cuddly-cards/Assets/Main/Script/Card/Animation/Child/FanToNoChildrenAnimation.cs
@@ -15,12 +15,16 @@ public class FanToNoChildrenAnimation : ChildParentAnimation
         return DOTween.Sequence()
             .Append(_subAnimations.FanInCard(activeNode, baseNode, false, false))
             .AppendInterval(_waitTime + _horizontalTime)
-            .Append(_subAnimations.MoveNodeYLowerPile(activeNode));
+            .Append(_subAnimations.MoveNodeY(activeNode, activeNode.GetNodeCount(CardTraversal.CONTEXT)));
     }
+
+    
 
     public override Tween AnimateOldChildren(CardNode activeNode, CardNode baseNode)
     {
         Sequence sequence = DOTween.Sequence();
+
+        sequence.Join(AnimateOldChildrenChildren(activeNode, baseNode));
 
         List<CardNode> childNodes = baseNode.Children;
 
@@ -47,9 +51,58 @@ public class FanToNoChildrenAnimation : ChildParentAnimation
         return sequence;
     }
 
+    public Tween AnimateOldChildrenChildren(CardNode activeNode, CardNode baseNode)
+    {
+        Sequence sequence = DOTween.Sequence();
+
+        foreach (CardNode child in baseNode.Children)
+        {
+            if (child == activeNode)
+            {
+                continue;
+            }
+
+            foreach (CardNode childChild in child.Children)
+            {
+                _cardManager.AddToTopLevel(childChild);
+
+                int height = childChild.GetNodeCountUpToNodeInPile(baseNode, CardTraversal.CONTEXT);
+
+                if (height > activeNode.GetNodeCountBelowNodeInPile(baseNode, CardTraversal.CONTEXT))
+                {
+                    height -= activeNode.GetNodeCount(CardTraversal.CONTEXT);
+                }
+
+                // Move to correct size, then follow what the oldchildren do
+                sequence.Join(DOTween.Sequence()
+                    .Append(_subAnimations.MoveNodeY(childChild, childChild.GetNodeCountUpToNodeInPile(baseNode, CardTraversal.CONTEXT)))
+                    .AppendInterval(_horizontalTime + _waitTime)
+                    .Append(_subAnimations.MoveNodeZFarther(childChild))
+                    .Append(_subAnimations.MoveNodeY(childChild, height))
+                    );
+
+            }
+        }
+        
+        return sequence;
+    }
+
     public override Tween AnimateChildren(CardNode activeNode, CardNode baseNode)
     {
-        return DOTween.Sequence();
+        Sequence sequence = DOTween.Sequence();
+
+        foreach (CardNode child in activeNode.Children)
+        {
+            _cardManager.AddToTopLevel(child);
+
+            sequence.Join(DOTween.Sequence()
+                .Append(_subAnimations.MoveNodeY(child, child.GetNodeCountUpToNodeInPile(baseNode, CardTraversal.CONTEXT)))
+                .AppendInterval(_horizontalTime + _waitTime + _horizontalTime)
+                .Append(_subAnimations.MoveNodeY(child, child.GetNodeCountUpToNodeInPile(activeNode, CardTraversal.CONTEXT)))
+                );
+        }
+
+        return sequence;
     }
 
 }
